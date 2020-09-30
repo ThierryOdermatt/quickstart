075f0e2efe4bb0f299ee95c90636d7fcd6ec2a7f
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2020-09-30 08:54:03 +0200

    Working through Tour of Heroes tutorial (till Multiple Components)

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,20 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'src/hero.dart';
+import 'src/mock_heroes.dart';
+
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, formDirectives],
 )
+
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of Heroes';
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
 }
